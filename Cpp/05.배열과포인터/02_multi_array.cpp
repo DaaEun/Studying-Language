21e321e3e6a0336c33765bc7d2ed1310b178a3f1
DaaEun <didekdms123@gmail.com>
2021-07-02 14:36:01 +0900

    다차원 배열 공부

---
+++ Cpp/05.배열과포인터/02_multi_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int arr1[2][3] = { 10, 20, 30, 40 };
+	int arr2[2][3] = {
+		{10, 20, 30},
+		{40, 50, 60}
+	};
+
+	cout << "arr1의 배열 요소의 값" << endl;
+	cout << arr1[0][0] << " ";
+	cout << arr1[0][1] << " ";
+	cout << arr1[0][2] << " ";
+	cout << arr1[1][0] << " ";
+	cout << arr1[1][1] << " ";
+	cout << arr1[1][2] << endl;
+
+	cout << "arr2의 배열 요소의 값" << endl;
+	cout << arr2[0][0] << " ";
+	cout << arr2[0][1] << " ";
+	cout << arr2[0][2] << " ";
+	cout << arr2[1][0] << " ";
+	cout << arr2[1][1] << " ";
+	cout << arr2[1][2];
+	return 0;
+}
