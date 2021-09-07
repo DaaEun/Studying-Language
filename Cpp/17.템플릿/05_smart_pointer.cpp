ec5720d03cd4cb1695860a45cc4e8ae2714b47e3
DaaEun <didekdms123@gmail.com>
2021-09-07 23:48:20 +0900

    17.템플릿

--- Cpp/17.템플릿/05_smart_pointer.cpp
+++ Cpp/17.템플릿/05_smart_pointer.cpp
@@ -21,3 +21,40 @@
 	- move() 멤버 함수를 통해 소유권을 이전할 수는 있지만, 복사할 수는 없다.
 	- 소유권이 이전되면, 이전 unique_ptr 인스턴스는 더는 해당 객체를 소유하지 않게 재설정된다.
 */
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Person
+{
+private:
+	string name_;
+	int age_;
+public:
+	Person(const string& name, int age);	// 기초 클래스 생성자의 선언 
+	~Person() { cout << "소멸자가 호출되었습니다." << endl; }
+	void ShowPersonInfo();
+};
+
+int main(void)
+{
+	shared_ptr<Person> hong = make_shared<Person>("길동", 29);
+	cout << "현재 소유자 수 : " << hong.use_count() << endl;	// 1
+	auto han = hong;
+	cout << "현재 소유자 수 : " << hong.use_count() << endl;	// 2
+	han.reset();		// shared_ptr인 han을 해제함. 
+	cout << "현재 소유자 수 : " << hong.use_count() << endl;	// 1
+	return 0;
+}
+
+Person::Person(const string& name, int age)	// 기초 클래스 생성자의 정의 
+{
+	name_ = name;
+	age_ = age;
+	cout << "생성자가 호출되었습니다." << endl;
+}
+
+void Person::ShowPersonInfo()
+{
+	cout << name_ << "의 나이는 " << age_ << "살입니다." << endl;
+}
