bbe9c3b52d9c1e13bf69e309ab6da968525a9d8a
DaaEun <didekdms123@gmail.com>
2021-10-18 23:54:02 +0900

    18.STL반복자

--- Cpp/18.STL반복자/02_test.cpp
+++ Cpp/18.STL반복자/02_test.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main(void)
 {
 	vector<int> vc = {1, 2, 3, 4, 5};
-	copy(vc.begin(), vc.end(), ostream_iterator<int>(cout));
+	//copy(vc.begin(), vc.end(), ostream_iterator<int>(cout));
 	cout << endl;
-	copy(vc.begin(), vc.end(), ostream_iterator<int>(cout, " "));
+	//copy(vc.begin(), vc.end(), ostream_iterator<int>(cout, " "));
 	return 0;
 }
