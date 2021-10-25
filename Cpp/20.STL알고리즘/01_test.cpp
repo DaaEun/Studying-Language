39f397e9b66a76c9adf30ba882e958508c8665e9
DaaEun <didekdms123@gmail.com>
2021-10-25 23:51:53 +0900

    20.STL알고리즘

---
+++ Cpp/20.STL알고리즘/01_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main(void)
+{
+	vector<int> vc = {20, 40, 10, 30};	// vector 객체의 선언 및 초기화 
+
+	sort(vc.begin(), vc.end(), greater<int>());
+	copy(vc.begin(), vc.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+	
+	sort(vc.begin(), vc.end(), less<int>());
+	copy(vc.begin(), vc.end(), ostream_iterator<int>(cout, " "));
+	return 0;
+}
