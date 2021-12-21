12de732117171b1b85dd8c33dabdc483e0495026
DaaEun <didekdms123@gmail.com>
2021-12-21 20:08:52 +0900

    20211221 SQL study

--- SQL/ch12_practice2.sql
+++ SQL/ch12_practice2.sql
@@ -3,3 +3,11 @@
 -- 단, 동점자에게 같은 순위를 주며, 이 경우 다음 순위와 동점자 수 만큼 차이가 나야 합니다.
 -- 출력 컬럼은 국가, 고객수, 순위 순으로 합니다.
 -- 결과는 순위의 오름차순, 국가의 오름차순으로 정렬합니다.
+
+
+SELECT	country, COUNT(*) 고객수,
+		RANK() OVER (ORDER BY COUNT(*) DESC) AS 순위			/* RANK() 함수를 계산할 때의 정렬 기준 */
+FROM 	s_customers
+GROUP 	BY country
+ORDER 	BY 순위, country;			/* 출력할 때의 정렬 기준. */
+									/* 이게 없으면, RANK() 함수의 정렬 기준대로 출력됨. */
--- SQL/ch12_practice3.sql
+++ SQL/ch12_practice3.sql
@@ -3,3 +3,12 @@
 -- 단, 동점자에게 같은 순위를 주며, 이 경우 다음 순위와 동점자 수 만큼 차이가 나야 합니다.
 -- 출력 컬럼은 orderNo, orderDate, 주문개수합계, 순위 순으로 합니다.
 -- 결과는 같은 주문년도 내에서, 주문개수합의 내림차순으로 정렬합니다.
+
+SELECT	orderNo, orderDate, SUM(quantity) AS 주문개수합계,
+		RANK() OVER
+		(
+			PARTITION BY YEAR(orderDate)
+			ORDER BY SUM(quantity) DESC
+		) AS 순위
+FROM	s_orders JOIN s_orderDetails USING (orderNo)
+GROUP 	BY orderNo;
