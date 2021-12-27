35b01388496d8db5430d2055bf11620b744781d9
DaaEun <didekdms123@gmail.com>
2021-12-27 23:16:09 +0900

    20211227 SQL study

--- SQL/ch12_practice6.sql
+++ SQL/ch12_practice6.sql
@@ -6,3 +6,23 @@
 -- 성명은 firstName과 lastName으로 구성하며, 사이에 공백 문자(space)가 하나 들어갑니다.
 -- 지점별 고객수(중계)를 출력할 때, 성명에는 '모든 직원'이라 출력합니다.
 -- 전체 고객수(합계)를 출력할 때, 지점에는 '모든 지점', 성명에는 '모든 직원'이라 출력합니다.
+
+WITH temp AS
+(
+		SELECT	COALESCE(O.city, '미정') AS 지점,
+				COALESCE(CONCAT(firstName, ' ', lastName), '미정') AS 성명, 
+				customerId
+		FROM	s_offices O JOIN s_employees USING (officeCode)
+				RIGHT JOIN s_customers ON employeeId = salesRepId
+)
+SELECT	CASE	GROUPING(지점)
+			WHEN 1 THEN '모든 지점' 
+			ELSE 지점
+		END AS 지점,
+		CASE	GROUPING(성명)
+			WHEN 1 THEN '모든 직원'
+			ELSE 성명
+		END AS 성명,
+		COUNT(customerId) AS 고객수
+FROM 	temp
+GROUP 	BY 지점, 성명 WITH ROLLUP;
